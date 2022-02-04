ecb51fde28a7e6da4a11e190d201dc5a6bb180b1
Artowod <myjavaname@gmail.com>
2022-02-04 23:49:17 +0200

    dd auth middl

--- routes/todos.js
+++ routes/todos.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const todosController = require("../controllers/todoController");
-const auth = require("../middlewares/authMiddleware");
+const auth = require("../middlewares/auth/authMiddleware");
 /*
 8. Реализовать энд - поинт создания карточки
 9. Реализовать энд-поинт редактирования карточки
@@ -13,20 +13,20 @@ const auth = require("../middlewares/authMiddleware");
 router.get("/", function (req, res, next) {
   res.send("Please use /active or  /completed to get respective set of cards.");
 });
-router.get("/active", todosController.getActiveTodos);
-router.get("/completed", todosController.getCompletedTodos);
+// router.get("/active", todosController.getActiveTodos);
+// router.get("/completed", todosController.getCompletedTodos);
 
-router.post("/add", todosController.addTodo);
-router.put("/update/:todoId", todosController.updateTodo);
-router.patch("/status/:todoId", todosController.setStatusTodo);
-router.delete("/remove/:todoId", todosController.removeTodo);
+// router.post("/add", todosController.addTodo);
+// router.put("/update/:todoId", todosController.updateTodo);
+// router.patch("/status/:todoId", todosController.setStatusTodo);
+// router.delete("/remove/:todoId", todosController.removeTodo);
 
-// router.get("/active", auth, todosController.getActiveTodos);
-// router.get("/completed", auth, todosController.getCompletedTodos);
+router.get("/active", auth, todosController.getActiveTodos);
+router.get("/completed", auth, todosController.getCompletedTodos);
 
-// router.post("/add", auth, todosController.addTodo);
-// router.put("/update/:todoId", auth, todosController.updateTodo);
-// router.patch("/status/:todoId", auth, todosController.setStatusTodo);
-// router.delete("/remove/:todoId", auth, todosController.removeTodo);
+router.post("/add", auth, todosController.addTodo);
+router.put("/update/:todoId", auth, todosController.updateTodo);
+router.patch("/status/:todoId", auth, todosController.setStatusTodo);
+router.delete("/remove/:todoId", auth, todosController.removeTodo);
 
 module.exports = router;
